c47fff1cee83a426030b91bdc135109b8820a6dc
kuchla <josni_kuchla@hotmail.com>
2019-07-14 23:11:35 -0300

    Created icons for sidebar

--- carneiro-app/config/adminlte.php
+++ carneiro-app/config/adminlte.php
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b> KUCHLA',
+    'logo' => '<b>Admin</b> CFCM',
 
     'logo_mini' => '<b>A</b>LT',
 
@@ -110,121 +110,50 @@ return [
     'menu' => [
         'Menu',
         [
-            'text' => 'Blog',
+            'text' => 'Menu',
             'url'  => 'admin/blog',
             'can'  => 'manage-blog',
         ],
         [
-            'text'        => 'Home',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text'        => 'Dashboard',
+            'url'         => 'admin',
+            'icon'        => 'dashboard',
         ],
         [
-            'text'        => 'News',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text'        => 'Cursos',
+            'url'         => 'admin/courses',
+            'icon'        => 'mortar-board',
         ],
         [
-            'text'        => 'Courses',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text'        => 'Noticias',
+            'url'         => 'admin/news',
+            'icon'        => 'newspaper-o',
         ],
         [
             'text'        => 'Links',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'url'         => 'admin/links',
+            'icon'        => 'link',
         ],
         [
-            'text'        => 'Gallery',
+            'text'        => 'Galeria',
             'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'icon'        => 'photo',
         ],
         [
-            'text'        => 'Administration',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text'        => 'Administração',
+            'url'         => 'admin/administrations',
+            'icon'        => 'users',
         ],
         [
-            'text'        => 'Institution',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text'        => 'Institucional',
+            'url'         => 'admin/institutions',
+            'icon'        => 'institution',
         ],
         
-        'ACCOUNT SETTINGS',
         [
-            'text' => 'Profile',
-            'url'  => 'admin/settings',
+            'text' => 'Usuários',
+            'url'  => 'admin/users',
             'icon' => 'user',
-        ],
-        [
-            'text' => 'Change Password',
-            'url'  => 'admin/settings',
-            'icon' => 'lock',
-        ],
-        [
-            'text'    => 'Multilevel',
-            'icon'    => 'share',
-            'submenu' => [
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'Level One',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'Level Two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'Level Two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        'LABELS',
-        [
-            'text'       => 'Important',
-            'icon_color' => 'red',
-        ],
-        [
-            'text'       => 'Warning',
-            'icon_color' => 'yellow',
-        ],
-        [
-            'text'       => 'Information',
-            'icon_color' => 'aqua',
         ],
     ],
 
--- carneiro-app/resources/views/admin/home/index.blade.php
+++ carneiro-app/resources/views/admin/home/index.blade.php
@@ -5,12 +5,9 @@
 @section('content_header')
 <h1>
     Dashboard
-    <small>Control panel</small>
+    <small>Index</small>
 </h1>
-<ol class="breadcrumb">
-    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-    <li class="active">Dashboard</li>
-</ol>
+
 @stop
 
 @section('content')
